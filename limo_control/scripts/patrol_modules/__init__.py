312784fd6802bc70ce8ad694d5aeffdf0dc1f82a
scandial <scandial@example.com>
2025-10-20 17:20:31 +0800

    Consolidate patrol_modules into scripts/patrol_modules; unify __init__ exports

---
+++ limo_control/scripts/patrol_modules/__init__.py
@@ -0,0 +1,24 @@
+﻿# patrol_modules consolidated package
+"""
+patrol_modules package (consolidated)
+
+Exports common classes for quick import patterns:
+  from patrol_modules import SensorHub, WallFollower, BackoffBehavior, StuckRecovery, ReturnHome, CoverageGrid
+Also contains dynamic_tracker, frontier_explore, lidar_avoid modules for avoidance nodes.
+"""
+
+from .sensors import SensorHub
+from .wall_follow import WallFollower
+from .backoff import BackoffBehavior
+from .stuck_recovery import StuckRecovery
+from .return_home import ReturnHome
+from .coverage_grid import CoverageGrid
+
+__all__ = [
+    "SensorHub",
+    "WallFollower",
+    "BackoffBehavior",
+    "StuckRecovery",
+    "ReturnHome",
+    "CoverageGrid",
+]
